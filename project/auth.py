7cc05332d4e631f823ac72f46e4eb79315c6025e
wajiharaza <wajiha.raza@students.mq.edu.au>
2025-05-06 18:41:15 +1000

    SQL query built from user-controlled sources

--- project/auth.py
+++ project/auth.py
@@ -38,10 +38,16 @@ def signup_post():
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
+    
+    signup_query = text("select * from user where email = :email")
 
-    user = db.session.execute(text('select * from user where email = "' + email +'"')).all()
-    if len(user) > 0: # if a user is found, we want to redirect back to signup page so user can try again
-        flash('Email address already exists')  # 'flash' function stores a message accessible in the template code.
+    user = db.session.execute(signup_query, {"email": email}).all()
+    if (
+        len(user) > 0
+    ): # if a user is found, we want to redirect back to signup page so user can try again
+        flash(
+            'Email address already exists'
+            )  # 'flash' function stores a message accessible in the template code.
         app.logger.debug("User email already exists")
         return redirect(url_for('auth.signup'))
 
