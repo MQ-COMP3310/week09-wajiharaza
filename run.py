14567dbf8aeb965142fccc6295a74af41438e144
wajiharaza <wajiha.raza@students.mq.edu.au>
2025-05-06 18:43:04 +1000

    Flask app is run in debug mode: changed true to false

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from project import create_app
 
 if __name__ == '__main__':
   app = create_app()
-  app.run(host = '127.0.0.1', port = 8001, debug=True) 
+  app.run(host = '127.0.0.1', port = 8001, debug=False) 
 
